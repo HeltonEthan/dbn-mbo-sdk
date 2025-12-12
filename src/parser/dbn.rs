30f340495dcdedf188e1c0aa4c6239ff6e3db457
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-11 23:11:43 -0500

    minor tweaks

--- src/parser/dbn.rs
+++ src/parser/dbn.rs
@@ -20,11 +20,11 @@ pub fn run<F: FnMut(&MboMsg) -> Option<Action>>(mut logic: F, cfg: &Config) -> R
     for path in file::get_files(&cfg)?.iter(){
         let mut dbn_stream = Decoder::from_zstd_file(path)?.decode_stream::<MboMsg>();
 
-        while let Ok(Some(mbo_msg)) = dbn_stream.next() {
+        while let Some(mbo_msg) = dbn_stream.next()? {
             if mbo_msg.ts_recv < start_unix { continue; }
             if mbo_msg.ts_recv > end_unix { break; }
 
-            let _action = logic(&mbo_msg);
+            logic(&mbo_msg);
         }
     }
 
