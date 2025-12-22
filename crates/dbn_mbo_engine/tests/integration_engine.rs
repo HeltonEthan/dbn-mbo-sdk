2f5e2dac9d2f3c2713a67bd82e6cc6d1f29d95e0
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-22 17:45:24 -0500

    small refactor

--- crates/dbn_mbo_engine/tests/integration_engine.rs
+++ crates/dbn_mbo_engine/tests/integration_engine.rs
@@ -1,6 +1,9 @@
 use chrono::NaiveDate;
 use dbn::record::MboMsg;
-use dbn_mbo_engine::{api::latency::UnitNormalLatency, prelude::*};
+use dbn_mbo_engine::{
+    api::{action::Request, latency::UnitNormalLatency},
+    prelude::*,
+};
 use std::path::PathBuf;
 
 // cargo test -p dbn_mbo_engine --test integration_engine engine_test --release
@@ -10,14 +13,14 @@ fn engine_test() -> anyhow::Result<()> {
     let cfg = Config::new(
         PathBuf::from("C:/Users/helto/GLBX-20250915-NGKNUL4VBG"),
         NaiveDate::from_ymd_opt(2025, 05, 12).unwrap(),
-        NaiveDate::from_ymd_opt(2025, 08, 11).unwrap(),
+        NaiveDate::from_ymd_opt(2025, 05, 24).unwrap(),
         0,
     );
     run(&cfg, || logic, || UnitNormalLatency::new(25_000_000, 1_000_000))?;
     Ok(())
 }
 
-fn logic(mbo: &MboMsg) -> Option<action::Request> {
+fn logic(mbo: &MboMsg) -> Option<Request> {
     _ = mbo;
     None
 }
