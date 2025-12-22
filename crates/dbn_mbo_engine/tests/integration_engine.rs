5dc66661e9d2f808c79b1eaf4fbcedf5c4112feb
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-22 00:09:12 -0500

    added comment

--- crates/dbn_mbo_engine/tests/integration_engine.rs
+++ crates/dbn_mbo_engine/tests/integration_engine.rs
@@ -4,12 +4,13 @@ use dbn_mbo_engine::{api::latency::UnitNormalLatency, prelude::*};
 use std::path::PathBuf;
 
 // cargo test -p dbn_mbo_engine --test integration_engine engine_test --release
+// samply record target\release\deps\integration_engine-d09ece3e58622256.exe
 #[test]
 fn engine_test() -> anyhow::Result<()> {
     let cfg = Config::new(
         PathBuf::from("C:/Users/helto/GLBX-20250915-NGKNUL4VBG"),
         NaiveDate::from_ymd_opt(2025, 05, 12).unwrap(),
-        NaiveDate::from_ymd_opt(2025, 05, 17).unwrap(),
+        NaiveDate::from_ymd_opt(2025, 05, 14).unwrap(),
         0,
     );
     let mut latency = UnitNormalLatency::new(25_000_000, 1_000_000);
