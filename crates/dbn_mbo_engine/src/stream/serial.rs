20f15b065d15e748ac444438918e77e44b848a41
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-24 18:05:03 -0500

    serial addition

--- crates/dbn_mbo_engine/src/stream/serial.rs
+++ crates/dbn_mbo_engine/src/stream/serial.rs
@@ -1,1 +1,39 @@
+use dbn::{
+    decode::{DecodeStream, dbn::Decoder},
+    record::MboMsg,
+};
+use fallible_streaming_iterator::FallibleStreamingIterator;
 
+use crate::{
+    config::Config,
+    stream::{Ext, process_dir},
+};
+
+#[allow(dead_code)]
+pub fn run(cfg: &Config) -> anyhow::Result<()> {
+    for (ext, path) in process_dir(cfg.dir.as_path(), cfg.start, cfg.end)? {
+        match ext {
+            Ext::Zst => {
+                let mut dbn_stream = Decoder::from_zstd_file(path)?.decode_stream::<MboMsg>();
+                while let Some(mbo) = dbn_stream.next()? {
+                    if mbo.ts_recv < cfg.start {
+                        continue;
+                    } else if mbo.ts_recv >= cfg.end {
+                        break;
+                    }
+                }
+            },
+            Ext::Dbn => {
+                let mut dbn_stream = Decoder::from_file(path)?.decode_stream::<MboMsg>();
+                while let Some(mbo) = dbn_stream.next()? {
+                    if mbo.ts_recv < cfg.start {
+                        continue;
+                    } else if mbo.ts_recv >= cfg.end {
+                        break;
+                    }
+                }
+            },
+        }
+    }
+    Ok(())
+}
